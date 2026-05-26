fd2d42724463c2fa2dba5cd988993fa5e0320e8c
Kuppe13 <eaa25maku@students.eaaa.dk>
2026-05-26 16:42:28 +0200

    added date & location til create&updatePage

--- src/pages/UpdatePage.jsx
+++ src/pages/UpdatePage.jsx
@@ -12,6 +12,8 @@ export default function UpdatePage() {
   const navigate = useNavigate();
   const [image, setImage] = useState("");
   const [caption, setCaption] = useState("");
+  const [date, setDate] = useState("");
+  const [location, setLocation] = useState("");
 
   useEffect(() => {
     async function getPost() {
@@ -19,6 +21,8 @@ export default function UpdatePage() {
       const data = await response.json();
       setImage(data[0].image);
       setCaption(data[0].caption);
+      setDate(data[0].date);
+      setLocation(data[0].location);
     }
 
     getPost();
@@ -33,6 +37,8 @@ export default function UpdatePage() {
       body: JSON.stringify({
         image: image.trim(),
         caption: caption.trim(),
+        date: date.trim(),
+        location: location.trim(),
       }),
     });
 
@@ -41,12 +47,12 @@ export default function UpdatePage() {
 
   return (
     <main className="app">
-      <h1 className="page-title">Update Post</h1>
+      <h1 className="page-title">Updater event</h1>
 
       <form className="post-form" onSubmit={handleSubmit}>
         <div className="form-grid">
           <div className="form-field">
-            <label htmlFor="image">Image URL</label>
+            <label htmlFor="image">Billede URL</label>
             <input
               id="image"
               name="image"
@@ -61,18 +67,40 @@ export default function UpdatePage() {
           </div>
 
           <div className="form-field">
-            <label htmlFor="caption">Caption *</label>
+            <label htmlFor="caption">Titel</label>
             <textarea
               id="caption"
               name="caption"
               rows="4"
-              placeholder="Write a caption for your post..."
+              placeholder="Skriv en titel til dit event..."
               value={caption}
               onChange={(event) => setCaption(event.target.value)}
               required
             />
           </div>
+
+          <div className="form-field"></div>
+          <label htmlFor="date">Dato</label>
+          <input
+            id="date"
+            name="date"
+            placeholder="Hvornår foregår dit event?"
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+            required
+          />
         </div>
+
+        <div className="form-field"></div>
+        <label htmlFor="location">Lokation</label>
+        <input
+          id="location"
+          name="location"
+          placeholder="Hvor foregår dit event?"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          required
+        />
 
         <div className="form-actions">
           <button type="submit" className="btn btn-primary">
